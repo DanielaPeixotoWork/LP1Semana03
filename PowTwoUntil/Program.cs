b2a8d0aa41180794c47a8c538aea5185b38a6b93
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-06 14:30:47 +0000

    PowTwoUntil finished

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    private static void PowersOf2Until5()
+    {
+        for (int i = 1; i <= (1 << 5); i = i << 1)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        PowersOf2Until5();
+    }
+}

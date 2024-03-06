ac4a8f7dd08fbda8cbe38e4c6e10690cc4a01f04
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-06 12:24:27 +0000

    Pedir 6 argumrntos ao utilizador

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        if (args.Length != 6)
+
+        {
+            Console.WriteLine("Forneça 6 argumentos:");
+            return;
+        }
+
+        }
+    }
+}

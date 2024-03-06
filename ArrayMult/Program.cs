54f73710470567234de79d308457b61dd81192ca
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-06 21:13:13 +0000

    ArrayMult fix of errors

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -5,43 +5,39 @@ namespace ArrayMult
     class Program
     {
         static void Main(string[] args)
-        {
-
-        if (args.Length != 6)
-
-        {
-            Console.WriteLine("Forneça 6 argumentos:");
-            return;
-        }
-
-        float[,] A = new float[2, 2];
-        float[] b = new float[2];
-
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                A[i, j] = float.Parse(args[i * 2 + j]);
-            }
-        }
-
-        for (int i = 0; i < 2; i++)
-        {
-            b[i] = float.Parse(args[4 + i]);
-        }
-
-        float[] result = new float[2];
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                result[i] += A[i, j] * b[j];
-            }
-        }
         
-        Console.WriteLine("A · b =");
-        Console.WriteLine($"[{result[0]}]");
-        Console.WriteLine($"[{result[1]}]");
+        {
+            float[,] A = new float[2, 2];
+            float[] b = new float[2];
+
+            A = new float[2, 2]
+
+            {
+
+                { float.Parse(args[0]), float.Parse(args[1]) },
+                { float.Parse(args[2]), float.Parse(args[3]) }
+
+            };
+
+            b[0] = float.Parse(args[4]);
+            b[1] = float.Parse(args[5]);
+            
+            float[] result = new float[2];
+
+            for (int i = 0; i < 2; i++)
+
+            {
+                for (int j = 0; j < 2; j++)
+                {      
+
+                    result[i] += A[i, j] * b[j];
+
+                }
+                
+            }
+
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
 
         }
     }

693301afa733ba81ab63b1e335960173421a6400
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-06 12:29:55 +0000

    Converte os valores para float

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -14,6 +14,19 @@ namespace ArrayMult
             return;
         }
 
+        float[,] A = new float[2, 2];
+        float[] b = new float[2];
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                A[i, j] = float.Parse(args[i * 2 + j]);
+            }
+        }
+
+        for (int i = 0; i < 2; i++)
+
         }
     }
 }

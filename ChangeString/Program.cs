493b1df8df583299f24271e7f18187d295d36e5b
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-06 12:15:35 +0000

    substitui por X e dá o resultado

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -12,6 +12,14 @@ namespace ChangeString
         Console.WriteLine("Digite um caractere:");
         char charToReplace = Console.ReadKey().KeyChar;
         Console.WriteLine();
+
+        string charAsString = char.ToString(charToReplace);
+
+
+        string resultString = inputString.Replace(charAsString, "X");
+
+
+        Console.WriteLine("A frase ficaria: " + resultString);
         }
     }
 }

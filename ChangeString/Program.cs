700fce50b4df5a54ccb1e2d9da880aed091c4f7a
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-06 12:13:16 +0000

    Pedir ao utilizador uma frase e um caracter

---
+++ ChangeString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Digite uma frase:");
+        string inputString = Console.ReadLine();
+
+        Console.WriteLine("Digite um caractere:");
+        char charToReplace = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        }
+    }
+}
